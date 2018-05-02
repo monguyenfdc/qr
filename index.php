652a8826608f444537bb3b1743d1052d04665983
pqhuy1987 <pqhuy1987@gmail.com>
2018-05-02 15:20:35 +0700

    Update sửa Code

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- mysql_connect('localhost','root',''); //ket noi databse
+ mysql_connect('localhost','root',''); //ket noi FDC
  mysql_select_db("qr" ); //lua chon database
   mysql_query("SET NAMES utf8");
  date_default_timezone_set("Asia/Ho_Chi_Minh");
