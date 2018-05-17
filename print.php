6c98907e41ceb8402a2846a2581bb243b3845dd2
monguyenfdc <mo.nguyen@fdcc.com.vn>
2018-05-17 09:44:08 +0700

    còn cái này a

--- print.php
+++ print.php
@@ -157,7 +157,7 @@ $row3 = mysql_fetch_assoc($query3) ;
 <td class='s6' colspan='2'>Ngày cấp:</td>
 <td class='s9' dir='ltr'>$today</td>
 <td class='s10' rowspan='3'><div style='width:107px;height:102px'>
-<img src='https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=http://fdc.giskill.com/index.php?cmnd={$row3['cmnd']}' height='102px' width='102px'/>
+<img src='https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=http://work.fdcc.com.vn:8081/index.php?cmnd={$row3['cmnd']}' height='102px' width='102px'/>
 </div></td>
 </tr>
 <tr style='height:35px;'>
