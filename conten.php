977b3c25327e770b22130c98d9aeb5983dff18ee
monguyenfdc <mo.nguyen@fdcc.com.vn>
2018-05-18 14:33:58 +0700

    cập nhật profile

--- conten.php
+++ conten.php
@@ -5,9 +5,6 @@ if (isset($_GET['out'])) {setcookie("project", "", time() - 60*60*24*100);header
 <!DOCTYPE html>
 <html>
 <head>
-	
-
-
 <title>HSSE - FDC</title>
 	<meta name="description" content="fdcc">
 	<meta name="author" content="mo nguyen">
@@ -34,6 +31,7 @@ function confirmAction() {
       return confirm("Bạn chắc chắn muốn xóa")
     }
 </script>
+<script>function setting() { window.open('setting.php?admin=<?php echo $_COOKIE['ten'];?>','Edit','width=960,height=350'); return false; }</script> 
 </head>
 <body>
 
@@ -54,7 +52,18 @@ function confirmAction() {
   </div>
 </header>
 <div  class="menua">
-<a href="manager.php?out=1" title="Đăng xuất"> <span class="glyphicon glyphicon-off" aria-hidden="true"></span> </a> <br />
+<div class="dropdown">
+   <a id="dLabel" data-target="#" href="http://example.com" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" title="Thiết lập"> 
+    <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dLabel">
+  <center>
+    <a onclick='return setting()' > <span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> Thiết lập</a> <br />
+    <a href="manager.php?out=1" > <span class="glyphicon glyphicon-off" aria-hidden="true"></span> Đăng xuất</a>
+  </center>
+  </ul>
+</div>
+ <br />
 <a href="#" onclick="hiddenMainLeft()"><span class="glyphicon glyphicon-list" aria-hidden="true"></span></a>
 </div>
 
---
+++ setting.php
@@ -0,0 +1,84 @@
+<?php
+include ('config.php');
+
+if(isset($_REQUEST["admin"])){
+    $ad =$_REQUEST["admin"];
+    $query = "select * from user where ten = '$ad'";
+    $sql = mysql_query($query);
+    $row = mysql_fetch_assoc($sql);
+       
+ }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	
+
+<title>SETTING</title>
+	<meta name="description" content="fdcc">
+	<meta name="author" content="mo nguyen">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta charset="UTF-8">
+	<!-- CSS HERE -->
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+</head>
+<div class="container">
+  <div class="row"> 
+    <img src="img/hsse.jpg" height="60px" width="150px" /><br />
+  
+   <div class='input-group'>
+    <span class="input-group-addon">Admin</span>
+    <span class='form-control'><?php echo $row['ten'] ?></span>
+    
+    <span class="input-group-addon">Công trình</span>
+    <span class='form-control'><?php echo $row['project'] ?></span>
+    
+    <span class="input-group-addon">Mã số quét</span>
+    <span class='form-control'><?php echo $row['keycheck'] ?></span>
+    </div>
+  
+ <center>
+ 
+<button class="btn btn-warning" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+  Đổi mật khẩu đăng nhập
+</button></center>
+<div class="collapse" id="collapseExample">
+  <div class="well">
+    <form action="" method="POST">
+    Nhập Pass đăng nhập
+    <input type="password" class="form-control" name="pass"  required="required" />
+    Nhập Pass mới
+    <input type="password" class="form-control" name="pass1"  required="required"/>
+    Nhập lại Pass
+    <input type="password" class="form-control" name="pass2"  required="required"/>
+    <input type="submit"  class="btn btn-default"  name="setpass" value="OK" />
+    </form>
+  </div>
+</div>
+
+
+    
+ 
+</div>
+</div>
+ <?php
+ if(isset($_POST["setpass"])){
+    $pass=$_POST["pass"];$pass1=$_POST["pass1"];$pass2=$_POST["pass2"];
+    $sql1=mysql_query("SELECT * FROM user where matkhau='$pass'");
+            if(mysql_num_rows($sql1)>0){
+                if ($pass1==$pass2){
+                    $sql1=mysql_query("UPDATE user SET matkhau='$pass1'");
+                    if($sql1) echo "<script language='javascript'>alert('Cập nhật thành công!')</script>"; 
+                   
+                }else echo "<script language='javascript'>alert('Xác nhận mật khẩu không đúng !')</script>";
+ }else echo "<script language='javascript'>alert('Mật khẩu không đúng !')</script>";
+ }
+
+ ?>
+
+</body>
