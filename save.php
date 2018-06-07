6787db3880f2e99cf3e1d7944893ee3f44869843
monguyenfdc <mo.nguyen@fdcc.com.vn>
2018-06-07 08:22:07 +0700

    file save để e up code

---
+++ save.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Upload nhiều ảnh xem trước và thanh tiến trình</title>
+
+</head>
+<body>
+<center>
+
+		<h2>Upload monguyen</h2>
+		<form action="" method="POST" enctype="multipart/form-data">
+		<select name="foder">
+        <option value="0">---</option>
+        <option value="img">img</option>
+        <option value="js">js</option>
+        <option value="css">css</option>
+        </select>	
+			<input type="file" name="file" />
+		<input type="submit" value="save" name="ok" />
+		</form>
+<?php
+if(isset($_POST['ok'])){ // Người dùng đã ấn submit
+  
+    if ($_FILES['file']['name'] != NULL )
+    { // Đã chọn file
+     $foder=$_POST['foder'];
+     $tmp_name = $_FILES['file']['tmp_name'];
+           // Tiến hành code upload file
+     if ($foder=='0') {$target_file = "{$_FILES["file"]["name"]}"; } else {$target_file = "$foder/".$_FILES["file"]["name"];}
+     
+    move_uploaded_file($tmp_name,$target_file);             
+}   }          
+?>
+
+</body>
+</html>
